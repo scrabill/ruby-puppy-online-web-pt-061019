554c01261812147437d9253fcf254b464b78f7d5
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-07-12 01:55:27 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,27 @@
 # Add your code here
+class Dog
+
+  attr_accessor :name # A writer and reader for each dog needs to be created, so that we can reference it in a later function
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self # Add each new dog, when it is created, to the array of all of the dogs
+  end
+
+  def self.all # Self is needed so that we're referring to the Dog class (all dog instances) and now *one* instance of a dog
+    @@all
+  end
+
+  def self.clear_all
+    @@all.clear # Please do not delete all of the dogs :()
+  end
+
+  def self.print_all # def print_all also works? Why is that?
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
+
+end
